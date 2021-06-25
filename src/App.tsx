0059df037c42fd88181dc149bef3a49e1096e1e4
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-06-25 18:23:33 -0300

    finalizando a sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world, my world!!!</h1>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        {/* Switch - chama apenas 1 rota */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
